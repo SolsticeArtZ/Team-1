be6ad1d911e21e31110033e57570488b35b35d27
John Vieira <johnvieira209@gmail.com>
2024-10-01 16:39:04 -0700

    updated

--- DP-Project/space_tigersapp/admin.py
+++ DP-Project/space_tigersapp/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Product
- 
+
 # Register your models here.
- 
 class ProductAdmin(admin.ModelAdmin):
-   list_display = ('name', 'price', 'stock')
+    list_display = ('name', 'price', 'stock')
 
 
 admin.site.register(Product)
+
+from django.contrib import admin
+from .models import Customer
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'lname', 'email', 'phone')
+    search_fields = ('fname', 'lname', 'email')
--- DP-Project/space_tigersapp/models.py
+++ DP-Project/space_tigersapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
 class Product(models.Model):
-   name = models.CharField(max_length=255)
-   price = models.FloatField()
-   stock = models.IntegerField()
-   image_url = models.CharField(max_length=2083)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
 
 class User(models.Model):
     first_name = models.CharField(max_length=30)
@@ -14,3 +15,14 @@ class User(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     avatar_url = models.URLField(max_length=2083)
+
+class Customer(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    mi = models.CharField(max_length=1, blank=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    avatar_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
