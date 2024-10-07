dbebae489179fa96f70e3fd3d6eca18148a61eee
Nate Klingbeil <111285112+nateklingbeil@users.noreply.github.com>
2024-10-07 15:53:29 -0700

    Update admin.py

--- DP-Project/space_tigersapp/admin.py
+++ DP-Project/space_tigersapp/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-from .models import Product
-
+from .models import Product, User
+ 
 # Register your models here.
+ 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
-
+   list_display = ('name', 'price', 'stock')
 
 admin.site.register(Product)
-
-from django.contrib import admin
-from .models import Customer
-
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('fname', 'lname', 'email', 'phone')
-    search_fields = ('fname', 'lname', 'email')
+#added 'user' page to /admin. delete created users here easily to purge sql database
+admin.site.register(User)
