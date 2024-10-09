aeb600aac1f673cf4859ffea7158ce9cb897b004
John Vieira <johnvieira209@gmail.com>
2024-10-09 13:33:53 -0700

    Update admin.py

--- DP-Project/space_tigersapp/admin.py
+++ DP-Project/space_tigersapp/admin.py
@@ -9,3 +9,8 @@ class ProductAdmin(admin.ModelAdmin):
 admin.site.register(Product)
 #added 'user' page to /admin. delete created users here easily to purge sql database
 admin.site.register(User)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'lname', 'email', 'phone')
+    search_fields = ('fname', 'lname', 'email')
